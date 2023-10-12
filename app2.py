1c02b56b127fe011f11e71da1e599cb9bc65650c
niikun <niikun0209@gmail.com>
2023-10-12 20:23:48 +0900

    chage  resize_and_pad

--- app2.py
+++ app2.py
@@ -30,44 +30,49 @@ def orient_image(img):
         pass
     return img
 
-def center_crop(img, desired_size):
-    w, h = img.size
-    desired_w, desired_h = desired_size
+# def center_crop(img, desired_size):
+#     w, h = img.size
+#     desired_w, desired_h = desired_size
 
-    # アスペクト比を計算
-    img_ratio = w / h
-    desired_ratio = desired_w / desired_h
+#     # アスペクト比を計算
+#     img_ratio = w / h
+#     desired_ratio = desired_w / desired_h
 
-    # リサイズするサイズを計算
-    if img_ratio > desired_ratio:
-        # 元の画像が横長の場合
-        resize_w = int(h * desired_ratio)
-        resize_h = h
-    else:
-        # 元の画像が縦長、または同じアスペクト比の場合
-        resize_w = w
-        resize_h = int(w / desired_ratio)
+#     # リサイズするサイズを計算
+#     if img_ratio > desired_ratio:
+#         # 元の画像が横長の場合
+#         resize_w = int(h * desired_ratio)
+#         resize_h = h
+#     else:
+#         # 元の画像が縦長、または同じアスペクト比の場合
+#         resize_w = w
+#         resize_h = int(w / desired_ratio)
 
-    img = img.resize((resize_w, resize_h))
+#     img = img.resize((resize_w, resize_h))
 
     # 画像の中央を目的のサイズにクロップ
-    left = (resize_w - desired_w) / 2
-    top = (resize_h - desired_h) / 2
-    right = left + desired_w
-    bottom = top + desired_h
-    img = img.crop((left, top, right, bottom))
+    # left = (resize_w - desired_w) / 2
+    # top = (resize_h - desired_h) / 2
+    # right = left + desired_w
+    # bottom = top + desired_h
+    # img = img.crop((left, top, right, bottom))
 
-    return img
+    # return img
 
 
 
 def process_image(base_img, img1, img2, img3, img4):
     base_img = base_img.copy()
 
-    img1 = center_crop(img1, (300, 250))
-    img2 = center_crop(img2, (250, 200))
-    img3 = center_crop(img3, (200, 150))
-    img4 = center_crop(img4, (65, 65))
+    # img1 = center_crop(img1, (300, 250))
+    # img2 = center_crop(img2, (250, 200))
+    # img3 = center_crop(img3, (200, 150))
+    # img4 = center_crop(img4, (65, 65))
+
+    img1 = resize_and_pad(img1, (300, 250))
+    img2 = resize_and_pad(img2, (250, 200))
+    img3 = resize_and_pad(img3, (200, 150))
+    img4 = resize_and_pad(img4, (65, 65))
 
     base_img.paste(img1, (68, 250))
     base_img.paste(img2, (400, 250))
@@ -77,6 +82,26 @@ def process_image(base_img, img1, img2, img3, img4):
     a4_size = (2100, 2970)  # A4サイズ
     base_img = base_img.resize(a4_size)
     return base_img
+
+def resize_and_pad(img, desired_size):
+    w, h = img.size
+    desired_w, desired_h = desired_size
+
+    # アスペクト比を維持してリサイズ
+    ratio = min(desired_w / w, desired_h / h)
+    new_w = int(w * ratio)
+    new_h = int(h * ratio)
+    img = img.resize((new_w, new_h))
+
+    # 新しい画像を生成して白で塗りつぶす
+    new_img = Image.new("RGB", desired_size, color="white")
+    
+    # 元の画像を中央に配置
+    x_offset = (desired_w - new_w) // 2
+    y_offset = (desired_h - new_h) // 2
+    new_img.paste(img, (x_offset, y_offset))
+
+    return new_img
 
 def get_image_download_link(img, filename="output.png", text="Download result image"):
     buffered = io.BytesIO()
