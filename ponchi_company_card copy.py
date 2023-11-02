69afe1dcea04092168273837b698e0f63f8aa491
niikun <niikun0209@gmail.com>
2023-11-02 19:20:40 +0900

    company card

---
+++ ponchi_company_card copy.py
@@ -0,0 +1,83 @@
+import streamlit as st
+from PIL import Image, ExifTags
+import io
+import base64
+
+def orient_image(img):
+    """EXIF情報を元に画像を正しいオリエンテーションに調整します"""
+    for orientation in ExifTags.TAGS.keys():
+        if ExifTags.TAGS[orientation] == 'Orientation':
+            break
+
+    try:
+        exif = img._getexif()
+        if exif is not None and orientation in exif:
+            if exif[orientation] == 2:
+                img = img.transpose(Image.FLIP_LEFT_RIGHT)
+            elif exif[orientation] == 3:
+                img = img.rotate(180)
+            elif exif[orientation] == 4:
+                img = img.rotate(180).transpose(Image.FLIP_LEFT_RIGHT)
+            elif exif[orientation] == 5:
+                img = img.rotate(-90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+            elif exif[orientation] == 6:
+                img = img.rotate(-90, expand=True)
+            elif exif[orientation] == 7:
+                img = img.rotate(90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+            elif exif[orientation] == 8:
+                img = img.rotate(90, expand=True)
+    except (AttributeError, KeyError, IndexError):
+        pass
+    return img
+
+
+def process_image(base_img, img1):
+    base_img = base_img.copy()
+
+    img1 = resize_and_pad(img1, (220, 220))
+
+    base_img.paste(img1, (80, 280))
+
+    # a5_size = (1480,2100)  # A5サイズ
+    # base_img = base_img.resize(a5_size)
+    return base_img
+
+def resize_and_pad(img, desired_size):
+    w, h = img.size
+    desired_w, desired_h = desired_size
+
+    # アスペクト比を維持してリサイズ
+    ratio = min(desired_w / w, desired_h / h)
+    new_w = int(w * ratio)
+    new_h = int(h * ratio)
+    img = img.resize((new_w, new_h))
+
+    # 新しい画像を生成して白で塗りつぶす
+    new_img = Image.new("RGB", desired_size, color="white")
+    
+    # 元の画像を左上に配置
+    new_img.paste(img, (0, 0))
+
+    return new_img
+
+    return new_img
+
+def get_image_download_link(img, filename="output.png", text="いんさつしてね！"):
+    buffered = io.BytesIO()
+    img.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+    return f'<a target="_blank" href="data:image/png;base64,{img_str}" download="{filename}">{text}</a>'
+
+st.title("ポンチしんぶん しゃいんしょ いんさつしょ")
+
+uploaded_img1 = st.file_uploader("あなたの しゃしん をアップロードしてください (1.jpg)", type="jpg")
+
+if all([uploaded_img1]):
+    img1 = orient_image(Image.open(uploaded_img1))
+
+    result = process_image(Image.open("ponchi_company_card.png"), img1)
+    st.image(result, caption="できた！", use_column_width=True)
+    
+    st.markdown(get_image_download_link(result), unsafe_allow_html=True)
+else:
+    st.image("ponchi_company_card.png", caption="いんさつまえ", use_column_width=True)
--- ponchi_company_card.py
+++ ponchi_company_card.py
@@ -4,7 +4,7 @@ import io
 import base64
 
 def orient_image(img):
-    """EXIF情報を元に画像を正しいオリエンテーションに調整します"""
+    """EXIF情報を元に画像を正しいオリエンテーションに調整します。"""
     for orientation in ExifTags.TAGS.keys():
         if ExifTags.TAGS[orientation] == 'Orientation':
             break
@@ -12,73 +12,48 @@ def orient_image(img):
     try:
         exif = img._getexif()
         if exif is not None and orientation in exif:
-            if exif[orientation] == 2:
-                img = img.transpose(Image.FLIP_LEFT_RIGHT)
-            elif exif[orientation] == 3:
-                img = img.rotate(180)
-            elif exif[orientation] == 4:
-                img = img.rotate(180).transpose(Image.FLIP_LEFT_RIGHT)
-            elif exif[orientation] == 5:
-                img = img.rotate(-90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+            if exif[orientation] == 3:
+                img = img.rotate(180, expand=True)
             elif exif[orientation] == 6:
                 img = img.rotate(-90, expand=True)
-            elif exif[orientation] == 7:
-                img = img.rotate(90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
             elif exif[orientation] == 8:
                 img = img.rotate(90, expand=True)
-    except (AttributeError, KeyError, IndexError):
+    except (AttributeError, KeyError, IndexError, TypeError):
         pass
     return img
 
+def center_crop_to_square(img, size):
+    w, h = img.size
+    min_side = min(w, h)
+    left = (w - min_side) / 2
+    top = (h - min_side) / 2
+    right = (w + min_side) / 2
+    bottom = (h + min_side) / 2
+    img = img.crop((left, top, right, bottom)).resize((size, size), Image.Resampling.LANCZOS)
+    return img
 
 def process_image(base_img, img1):
     base_img = base_img.copy()
-
-    img1 = resize_and_pad(img1, (100, 100))
- 
-
-    base_img.paste(img1, (40, 40))
-
-    a5_size = (1480, 2100)  # A5サイズ
-    base_img = base_img.resize(a5_size)
+    img1 = center_crop_to_square(img1, 220)
+    base_img.paste(img1, (80, 280))
     return base_img
 
-def resize_and_pad(img, desired_size):
-    w, h = img.size
-    desired_w, desired_h = desired_size
-
-    # アスペクト比を維持してリサイズ
-    ratio = min(desired_w / w, desired_h / h)
-    new_w = int(w * ratio)
-    new_h = int(h * ratio)
-    img = img.resize((new_w, new_h))
-
-    # 新しい画像を生成して白で塗りつぶす
-    new_img = Image.new("RGB", desired_size, color="white")
-    
-    # 元の画像を左上に配置
-    new_img.paste(img, (0, 0))
-
-    return new_img
-
-    return new_img
-
-def get_image_download_link(img, filename="output.png", text="いんさつしてね！"):
+def get_image_download_link(img, filename="output.png", text="ダウンロード"):
     buffered = io.BytesIO()
     img.save(buffered, format="PNG")
     img_str = base64.b64encode(buffered.getvalue()).decode()
-    return f'<a target="_blank" href="data:image/png;base64,{img_str}" download="{filename}">{text}</a>'
+    href = f'<a href="data:image/png;base64,{img_str}" download="{filename}">{text}</a>'
+    return href
 
 st.title("ポンチしんぶん しゃいんしょ いんさつしょ")
 
-uploaded_img1 = st.file_uploader("あなたの しゃしん をアップロードしてください (1.jpg)", type="jpg")
+uploaded_img1 = st.file_uploader("あなたの写真をアップロードしてください", type=["jpg", "jpeg"])
 
-if all([uploaded_img1]):
-    img1 = orient_image(Image.open(uploaded_img1))
-
-    result = process_image(Image.open("ponchi2.png"), img1)
+if uploaded_img1:
+    img1 = Image.open(uploaded_img1)
+    img1 = orient_image(img1)
+    result = process_image(Image.open("ponchi_company_card.png"), img1)
     st.image(result, caption="できた！", use_column_width=True)
-    
     st.markdown(get_image_download_link(result), unsafe_allow_html=True)
 else:
-    st.image("ponchi2.png", caption="いんさつまえ", use_column_width=True)
+    st.image("ponchi_company_card.png", caption="いんさつまえ", use_column_width=True)
